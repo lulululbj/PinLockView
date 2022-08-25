47cd23968c21215e70cde4cd6624e3489e7b2752
luyao <sunluyao1993x@gmail.com>
2022-08-25 23:34:35 +0800

    migrate androidx

--- app/src/main/java/com/andrognito/pinlockviewapp/SampleActivity.java
+++ app/src/main/java/com/andrognito/pinlockviewapp/SampleActivity.java
@@ -1,8 +1,8 @@
 package com.andrognito.pinlockviewapp;
 
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
@@ -43,8 +43,8 @@ public class SampleActivity extends AppCompatActivity {
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_sample);
 
-        mPinLockView = (PinLockView) findViewById(R.id.pin_lock_view);
-        mIndicatorDots = (IndicatorDots) findViewById(R.id.indicator_dots);
+        mPinLockView = findViewById(R.id.pin_lock_view);
+        mIndicatorDots = findViewById(R.id.indicator_dots);
 
         mPinLockView.attachIndicatorDots(mIndicatorDots);
         mPinLockView.setPinLockListener(mPinLockListener);
